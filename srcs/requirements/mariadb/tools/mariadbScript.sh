0121b046cfefed8af2d53432d93aabd9ab800515
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-03-04 02:59:26 +0100

    adding mysql_secure_installation to the mariadb script

--- srcs/requirements/mariadb/tools/mariadbScript.sh
+++ srcs/requirements/mariadb/tools/mariadbScript.sh
@@ -1,7 +1,23 @@
 #/bin/sh
 
+#start the service of mariadb
 systemctl start mariadb
 systemctl enable mariadb
+
+#secure the mariadb installation
+mysql_secure_installation <<EOF
+y
+rootPassword
+rootPassword
+y
+y
+y
+y
+y
+EOF
+
+#create database, user and password, grant privileges, flush privileges
+
 
 
 
