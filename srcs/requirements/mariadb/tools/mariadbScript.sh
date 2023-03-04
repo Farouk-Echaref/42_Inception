2209263576225a6ebe4939e3330a1db2c5a7adca
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-03-04 04:54:39 +0100

    update localhost of user in mariadb database

--- srcs/requirements/mariadb/tools/mariadbScript.sh
+++ srcs/requirements/mariadb/tools/mariadbScript.sh
@@ -20,8 +20,8 @@ EOF
 # % character is a wildcard that matches any hostname or IP address
 mysql -u root -prootPassword <<EOF
 CREATE DATABASE mydb;
-CREATE USER 'fech-cha'@'%' IDENTIFIED BY 'fech-cha';
-GRANT ALL PRIVILEGES ON mydb.* TO 'fech-cha'@'%';
+CREATE USER 'fech-cha'@'198.168.1.2' IDENTIFIED BY 'fech-cha';
+GRANT ALL PRIVILEGES ON mydb.* TO 'fech-cha'@'198.168.1.2';
 FLUSH PRIVILEGES;
 EOF
 
