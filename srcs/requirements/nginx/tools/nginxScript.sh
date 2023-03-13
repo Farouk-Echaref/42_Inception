3d77f637204abc393de5c851cdbc01c594729950
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-03-14 00:42:56 +0100

    updating the server_name for nginx

--- srcs/requirements/nginx/tools/nginxScript.sh
+++ srcs/requirements/nginx/tools/nginxScript.sh
@@ -3,9 +3,9 @@
     openssl dhparam -out /etc/nginx/dhparam.pem 2048 && \
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/nginx/cert.key -out /etc/nginx/cert.crt \
-    -subj "/C=MA/ST=None/L=Khouribga/O=42Network/OU=1337Kh/CN=fech-cha.42.com" && \
+    -subj "/C=MA/ST=None/L=Khouribga/O=42Network/OU=1337Kh/CN=fech-cha.42.fr" && \
     openssl ecparam -genkey -name secp384r1 -out /etc/nginx/eckey.pem && \
-    openssl req -new -x509 -sha256 -key /etc/nginx/eckey.pem -out /etc/nginx/eccert.pem -days 365 -subj "/C=MA/ST=None/L=Khouribga/O=42Network/OU=1337Kh/CN=fech-cha.42.com"
+    openssl req -new -x509 -sha256 -key /etc/nginx/eckey.pem -out /etc/nginx/eccert.pem -days 365 -subj "/C=MA/ST=None/L=Khouribga/O=42Network/OU=1337Kh/CN=fech-cha.42.fr"
 
 
 nginx -g "daemon off;"
