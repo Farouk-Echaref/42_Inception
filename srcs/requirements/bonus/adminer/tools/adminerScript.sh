c3843433103316016147dd8c4affae173a33eb3b
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-05-06 08:01:30 -0400

    Inception Bonus: setting up adminer container for database

--- srcs/requirements/bonus/adminer/tools/adminerScript.sh
+++ srcs/requirements/bonus/adminer/tools/adminerScript.sh
@@ -1,11 +1,14 @@
 #!/bin/sh 
 
+#download the Adminer installation file from their official website, save the php file in /var/www/html
 wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer.php 
+
+#change the permission of the downloaded file
 chown -R www-data:www-data /var/www/html/adminer.php 
-chmod 755 /var/www/html/adminer.php
+chmod 777 /var/www/html/adminer.php
 
 
 cd /var/www/html
 
 rm -rf index.html
-php -S 0.0.0.0:80
+php -S 0.0.0.0:1234 adminer.php
