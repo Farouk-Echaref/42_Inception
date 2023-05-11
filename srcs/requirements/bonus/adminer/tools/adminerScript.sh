b558f74be4f4cde4323c0b25d9309f8c6d3e29ce
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-05-11 14:13:33 -0400

    update comments for adminer config

--- srcs/requirements/bonus/adminer/tools/adminerScript.sh
+++ srcs/requirements/bonus/adminer/tools/adminerScript.sh
@@ -7,8 +7,11 @@ wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer.php
 chown -R www-data:www-data /var/www/html/adminer.php 
 chmod 777 /var/www/html/adminer.php
 
-
+#change to the working directory
 cd /var/www/html
 
+#remove the default index
 rm -rf index.html
+
+# starts a built-in web server that listens on port 1234 and serves the Adminer PHP file
 php -S 0.0.0.0:1234 adminer.php
