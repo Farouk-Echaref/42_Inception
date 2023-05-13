271821ba416507ae68a9b041fdeb421c4ef4c6a7
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-05-13 16:05:20 -0400

    update mariadb dockerfile and script configs

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -6,28 +6,49 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 
 #Sets the owner of the /var/lib/mysql directory and all its contents to the mysql user and group
         chown -R mysql:mysql /var/lib/mysql
+        chmod -R 777 /var/lib/mysql
 
         # init database
-        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+        # mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+        mariadb-install-db --user=root --basedir=/usr --datadir=/var/lib/mysql \
+		--rpm --auth-root-authentication-method=normal \
+		--skip-test-db \
+		--default-time-zone=SYSTEM --enforce-storage-engine= \
+		--skip-log-bin \
+		--expire-logs-days=0 \
+		--loose-innodb_buffer_pool_load_at_startup=0 \
+		--loose-innodb_buffer_pool_dump_at_shutdown=0
+
 fi
+
+SQL1="CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+SQL2="CREATE USER IF NOT EXISTS '${DB_USER}'@'${DB_HOST}' IDENTIFIED BY '${DB_USER_PASS}';"
+SQL3="GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'${DB_HOST}';"
+SQL4="FLUSH PRIVILEGES;"
 
 #check if wordpress DB laready exist,if not create new one
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
+# if [ ! -d "/var/lib/mysql/wordpress" ]; then
 
-    #redirect the output of the following text block to the file
-        cat << EOF > /tmp/create_db.sql
-USE mysql;
-FLUSH PRIVILEGES;
-DELETE FROM     mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';
-CREATE DATABASE $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '$DB_USER'@'%' IDENTIFIED by '$DB_USER_PASS';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
-FLUSH PRIVILEGES;
-EOF
-        /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-        rm -f /tmp/create_db.sql
-fi
+        /usr/bin/mysqld --user=root & #--bootstrap < /tmp/create_db.sql
+        MY_SQL_PID=$!
+        # until mariadb -u mysql -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}" 2>&1 > /dev/null
+        # do
+        #         echo "Not Connected!"
+                sleep 4
+        # done
+        #redirect the output of the following text block to the file
+#         cat << EOF > /tmp/create_db.sql
+# CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+# CREATE USER IF NOT EXISTS '${DB_USER}'@'${DB_HOST}' IDENTIFIED BY '${DB_USER_PASS}';
+# GRANT ALL PRIVILEGES ON '${DB_NAME}'.* TO '${DB_USER}'@'${DB_HOST}';
+# FLUSH PRIVILEGES;
+# EOF
+
+echo "$SQL1" > /tmp/create_db.sql
+echo "$SQL2" >> /tmp/create_db.sql
+echo "$SQL3" >> /tmp/create_db.sql
+echo "$SQL4" >> /tmp/create_db.sql
+
+mysql -u root < /tmp/create_db.sql
+wait $MY_SQL_PID
+# fi
