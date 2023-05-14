66a17d7f4342594f90d179569263347c1d90334b
Farouk-Data <farouk.echcharef@usmba.ac.ma>
2023-05-13 21:02:29 -0400

    update nginx: from debian => to alpine

--- srcs/requirements/nginx/tools/nginxScript.sh
+++ srcs/requirements/nginx/tools/nginxScript.sh
@@ -9,9 +9,10 @@
 #     openssl req -new -x509 -sha256 -key /etc/nginx/eckey.pem -out /etc/nginx/eccert.pem -days 365 -subj "/C=MA/ST=None/L=Khouribga/O=42Network/OU=1337Kh/CN=fech-cha.42.fr"
 
 
-
-
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=MO/L=KH/O=1337/OU=student/CN=fech-cha.42.ma"
+
+# addgroup --gid 82 --system www-data
+# useradd -u 82  --system --group www-data www-data
 
 addgroup -g 82 -S www-data 
 adduser -u 82 -D -S -G www-data www-data
