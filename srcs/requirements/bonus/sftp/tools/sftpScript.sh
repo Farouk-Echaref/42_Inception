f94c623cc895cfd2e4074e4685b3160e4af74397
Farouk-Data <farouk.echcharef@usmba.ma>
2023-05-18 15:42:19 -0400

    update redis and sftp

--- srcs/requirements/bonus/sftp/tools/sftpScript.sh
+++ srcs/requirements/bonus/sftp/tools/sftpScript.sh
@@ -9,7 +9,7 @@ adduser $FTP_USER --disabled-password
 echo "$FTP_USER:$FTP_PASSWD" | chpasswd
 
 # set the ownership of the wordpress website files to FTP_USER
-chown -R $FTP_USER:$FTP_USER /var/www/html/wordpress
+chown -R $FTP_USER:$FTP_USER /var/www/html/
 
 
 #add user to ftp users list
