9faaf5bfe376bc83d8102c6a1275460b61a9b0b3
Farouk-Data <farouk.echcharef@usmba.ma>
2023-05-18 15:50:24 -0400

    update redis

--- srcs/requirements/bonus/redis/shell/redisScript.sh
+++ srcs/requirements/bonus/redis/shell/redisScript.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+
+sed -i 's/# bind 127.0.0.1/bind 0.0.0.0/g' /etc/redis/redis.conf
 echo maxmemory 256mb >> /etc/redis/redis.conf
 
 echo maxmemory-policy allkeys-lfu >> /etc/redis/redis.conf
