7dabe5e46158582532bc7d26066cba06ad50e4d8
Farouk ECH-CHAREF <farouk.echcharef@usmba.ac.ma>
2023-05-18 18:00:45 +0100

    update mariadb

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -12,6 +12,8 @@ mysql -u root -p$DB_ROOT_PASS -e "FLUSH PRIVILEGES;"
 
 mysql -u root -p$DB_ROOT_PASS -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';"
 
+mysql -u root -p$DB_ROOT_PASS -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
+
 mysql -u root -p$DB_ROOT_PASS -e "FLUSH PRIVILEGES;"
 
 kill `cat /var/run/mysqld/mysqld.pid`
